2123baa9092148c69c25d1ef0f71d6d795f96d62
John Crooks <johnmcrooks@gmail.com>
2016-05-31 17:16:07 -0400

    Week 3 day 2 Assignment: Completed Optional assignments. Load Contacts.json on initialization.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
+import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
 import java.io.File;
@@ -16,6 +17,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable {
     ObservableList<Contact> contacts = FXCollections.observableArrayList();
@@ -35,13 +37,11 @@ public class Controller implements Initializable {
             nameField.clear();
             emailField.clear();
             numberField.clear();
-            writeToFile(contact);
+            writeToFile(contacts);
         }else  {
-            System.out.println(" You won't see this but You can't leave any fields blank!!! ");
+            System.out.println("You won't see this but You can't leave any fields blank!!! ");
         }
-
     }
-
     public void onRemove(){
         SelectionModel model = list.getSelectionModel();
         Contact contact = (Contact) model.getSelectedItem();
@@ -50,13 +50,37 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        contacts = readfile();
         list.setItems(contacts);
     }
 
-    public void writeToFile(Contact contact) throws IOException {
+    public ObservableList<Contact> readfile(){
+            File f = new File("Contacts.json");
+            try {
+                Scanner scanner = new Scanner(f);
+                scanner.useDelimiter("\\Z");
+                String contents = scanner.next();
+                JsonParser parser = new JsonParser();
+
+                ArrayList<HashMap<String, String>> list = parser.parse(contents);               //data is parsed in as an ArrayList of Hashmaps for whatever reason
+                ObservableList<Contact> contacts = FXCollections.observableArrayList();         //We want the data to be added to an Observable Array list for use
+
+                for (HashMap<String, String> h: list){                                          //For every Hashmap in the Array List....
+                    contacts.add(new Contact(h.get("name"), h.get("email"), h.get("phone")));   //Create A Contact object with variables drawn from each of the Hashmap<Strings,Strings>
+                }
+                return contacts;
+
+            }catch (Exception e) {
+                e.printStackTrace();
+                return contacts;
+            }
+    }
+
+
+    public void writeToFile(ObservableList<Contact> contacts) throws IOException {
         String fileName = "Contacts.json";
         JsonSerializer serializer = new JsonSerializer();
-        String json = serializer.serialize(contact);
+        String json = serializer.serialize(contacts);
         File f = new File(fileName);
         FileWriter fw = new FileWriter(f);
         fw.write(json);
